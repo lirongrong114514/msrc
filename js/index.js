6b8ea62189cbbe2f523bfc9747f28514a680a43f
lirongrong114514 <145199268+lirongrong114514@users.noreply.github.com>
2024-05-25 17:52:14 +0800

    update for code

--- js/index.js
+++ js/index.js
@@ -6,3 +6,16 @@ function image(){
     var x = document.getElementById("img");
     x.innerHTML = image/errorimg.jpg;
 }
+function home(){
+    window.locationbar.href = "index.html";
+}
+
+
+function igoto1(){
+    var x = document.getElementById("igoto1");
+    x.innerText = "官方QQ群：916903857\n一服（2b2t）ip：2b2t.msrc.online:30112\n二服（生电）ip：play.msrc.online:30122";
+}
+function igoto2(){
+    var x = document.getElementById("igoto2");
+    x.innerText = "carpet/carpet-tis/领地（使用指南：https://www.mcmod.cn/class/3201.html/简单登录/星光/锂）"
+}
